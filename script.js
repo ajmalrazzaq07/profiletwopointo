5b695dcd29fa21cf551e2b823797e66403a0347d
Ajmal Razzaq Bhatti <ajmalrazzaq07@gmail.com>
2023-05-28 00:37:28 +0500

    added splide js

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-// Decrease scroll speed
-function decreaseScrollSpeed(event) {
-  // Change the scroll speed factor according to your preference
-  const scrollSpeedFactor = 0.5;
+document.addEventListener("DOMContentLoaded", function () {
+  var splide = new Splide(".splide", {
+    direction: "ttb", // top to bottom
+    height: "100vh", // Adjust this value to control slide height
+    arrows: true,
+    pagination: false,
+    cover: true,
+    interval: 4000,
+    drag: "free",
+    snap: true,
+    speed: 1000,
+  }).mount();
 
-  // Calculate the new scroll position
-  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+  splide.on("moved", function () {
+    var arrowDown = document.querySelector(".splide__arrow--next");
 
-  const newScrollTop = scrollTop + event.deltaY * scrollSpeedFactor;
-  const newScrollLeft = scrollLeft + event.deltaX * scrollSpeedFactor;
-
-  // Scroll to the new position
-  window.scrollTo({
-    top: newScrollTop,
-    left: newScrollLeft,
-    behavior: "smooth", // Use 'auto' for instant scroll without animation
+    // Show arrow down on the first slide and hide it on others
+    if (splide.index === 0) {
+      arrowDown.style.display = "block";
+    } else {
+      arrowDown.style.display = "none";
+    }
   });
+  var bar = document.querySelector(".my-slider-progress-bar");
 
-  // Prevent the default scroll behavior
-  event.preventDefault();
-}
-
-// Attach the event listener to the 'wheel' event
-window.addEventListener("wheel", decreaseScrollSpeed);
+  // Updates the bar width whenever the carousel moves:
+  splide.on("moved", function () {
+    var end = splide.Components.Controller.getEnd() + 1;
+    var rate = Math.min((splide.index + 1) / end, 1);
+    bar.style.width = String(100 * rate) + "%";
+  });
+});
