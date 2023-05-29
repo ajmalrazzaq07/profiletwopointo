057d2335fb950aa2877caf00d74dc365a862e2f1
Ajmal Razzaq Bhatti <ajmalrazzaq07@gmail.com>
2023-05-29 18:30:22 +0500

    fixed

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
-var arrowDown = document.querySelector(".splide__arrow--next");
-var image = document.getElementById("image");
-var nav = document.querySelector("nav");
-nav.style.display = "none";
-document.addEventListener("DOMContentLoaded", function () {
-  var splide = new Splide(".splide", {
-    direction: "ttb", // top to bottom
-    height: "90vh", // Adjust this value to control slide height
-    arrows: true,
-    pagination: false,
-    cover: true,
-    interval: 4000,
-    drag: "free",
-    snap: true,
-    speed: 1000,
-  }).mount();
+// set options when initializing butter.js
+butter.init({ cancelOnTouch: false });
 
-  splide.on("moved", function () {
-    // Show arrow down on the first slide and hide it on others
-    image.classList.add("flicker-in-1");
-    if (splide.index === 0) {
-      image.classList.remove("flicker-in-1");
-      arrowDown.style.display = "";
-      nav.style.display = "none";
-    } else {
-      arrowDown.style.display = "none";
-      nav.style.display = "";
-    }
-  });
-});
+setTimeout(() => {
+  document.getElementById("loading").style.display = "none";
+  AOS.init();
+}, 2000);
