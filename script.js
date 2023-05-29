e4f8c38f03f4f9dcf5f4eff589be9cb71c3fa035
Ajmal Razzaq Bhatti <ajmalrazzaq07@gmail.com>
2023-05-29 12:33:56 +0500

    added animations

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var arrowDown = document.querySelector(".splide__arrow--next");
+var image = document.getElementById("image");
 var nav = document.querySelector("nav");
 nav.style.display = "none";
 document.addEventListener("DOMContentLoaded", function () {
@@ -16,7 +17,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
   splide.on("moved", function () {
     // Show arrow down on the first slide and hide it on others
+    image.classList.add("flicker-in-1");
     if (splide.index === 0) {
+      image.classList.remove("flicker-in-1");
       arrowDown.style.display = "";
       nav.style.display = "none";
     } else {
