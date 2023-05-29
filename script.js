1433267e4008b42ba362df3be08ee74fe8a6cd8d
Ajmal Razzaq Bhatti <ajmalrazzaq07@gmail.com>
2023-05-29 20:50:21 +0500

    added pre-loading

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ setTimeout(() => {
   document.getElementById("loading").style.display = "none";
   AOS.init();
 }, 2000);
+function slide() {
+  var divElement = document.getElementById("sectionabout");
+
+  var offsetTop = divElement.offsetTop;
+
+  if (typeof butter !== "undefined" && butter.active) {
+    butter.cancel();
+  }
+
+  var scrollOptions = {
+    top: offsetTop,
+    behavior: "smooth",
+  };
+
+  window.scrollTo(scrollOptions);
+
+  var observer = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        if (typeof butter !== "undefined" && !butter.active) {
+          butter.init();
+        }
+        observer.unobserve(entry.target);
+      }
+    });
+  });
+
+  observer.observe(divElement);
+}
