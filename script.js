bc631a8bc25053979350ff01a2fd096d9f30eeae
Ajmal Razzaq Bhatti <ajmalrazzaq07@gmail.com>
2023-06-05 16:48:29 +0500

    updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // set options when initializing butter.js
-butter.init({ cancelOnTouch: false });
+butter.init({ cancelOnTouch: true });
 
 setTimeout(() => {
   document.getElementById("loading").style.display = "none";
@@ -7,7 +7,6 @@ setTimeout(() => {
 }, 2000);
 function slide() {
   var divElement = document.getElementById("sectionabout");
-
   var offsetTop = divElement.offsetTop;
 
   if (typeof butter !== "undefined" && butter.active) {
@@ -25,7 +24,7 @@ function slide() {
     entries.forEach(function (entry) {
       if (entry.isIntersecting) {
         if (typeof butter !== "undefined" && !butter.active) {
-          butter.init();
+          butter.init({ cancelOnTouch: true });
         }
         observer.unobserve(entry.target);
       }
@@ -33,4 +32,46 @@ function slide() {
   });
 
   observer.observe(divElement);
+  // Check if the device is mobile
 }
+var isMobile =
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
+
+// Add or remove the onclick attribute based on device type
+var scrollLink = document.getElementById("scrollLink");
+if (isMobile) {
+  scrollLink.removeAttribute("onclick");
+  window.scrollBy(0, -800);
+  // Set scroll behavior to smooth
+  document.documentElement.style.scrollBehavior = "smooth";
+} else {
+  scrollLink.setAttribute("onclick", "slide()");
+}
+
+fetch("https://api.github.com/user/repos", {
+  headers: {
+    Authorization: "Bearer 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",
+  },
+})
+  .then((response) => response.json())
+  .then((repositories) => {
+    const repositoriesElement = document.getElementById("repositories");
+    repositories.forEach((repository) => {
+      console.log(repositories);
+      const repoElement = document.createElement("div");
+      repoElement.classList.add("projectssection__portfolio__card");
+      repoElement.innerHTML = `
+          <h3>${repository.name}</h3>
+          <p>${repository.description}</p>
+          <div class="row">
+          <a href="${repository.html_url}" target="_blank">View on</a>
+          <a href="${repository.homepage}" target="_blank">Website</a>
+          </div>
+          
+        `;
+      repositoriesElement.appendChild(repoElement);
+    });
+  })
+  .catch((error) => console.error(error));
